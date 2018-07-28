0c6824bda8ea78ccc07ec1e39025696cb4028214
changrui <979922105@qq.com>
2018-07-28 18:14:04 +0800

    添加个人中心以及佣金提现页面

---
+++ js/put_forward.js
@@ -0,0 +1,20 @@
+$(function(){
+	$('.btn').click(function(){
+		var name = $('#name').val(),
+			bank = $('#bank').val(),
+			banknum = $('#banknum').val(),
+			bankp = $('#bankp').val(),
+			bankc = $('#bankc').val(),
+			bankadd = $('#bankadd').val(),
+			tel = $('#tel').val(),
+			paypassword = $('#paypassword').val(),
+			qq = $('#qq').val();
+			if(name == ''){
+				alert('请输入您的真实姓名');
+				return false;
+			}else if(bank == '0'){
+				alert('请选择开户行');
+				return false;
+			}
+	})
+})
