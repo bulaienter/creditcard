43ad6c68408a57f3c9fab764df887e8b26fdfc2f
changrui <979922105@qq.com>
2018-08-03 18:17:52 +0800

    弹窗

--- js/put_forward.js
+++ js/put_forward.js
@@ -10,10 +10,18 @@ $(function(){
 			paypassword = $('#paypassword').val(),
 			qq = $('#qq').val();
 			if(name == ''){
-				alert('请输入您的真实姓名');
+				myConfirm('请输入您的真实姓名','取消','确定','1',function(res){
+					if(res.status){	
+						//用户点击确定
+					}else {
+						//用户点击取消
+					}
+				});
 				return false;
 			}else if(bank == '0'){
-				alert('请选择开户行');
+				myConfirm('请输入您的银行卡号','取消','确定','1',function(res){
+					
+				});
 				return false;
 			}
 		
